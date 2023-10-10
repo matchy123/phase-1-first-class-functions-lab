fa5fd5fb3a45eafa6efdd6508f0a7f5da7001c7c
matchy123 <dikomohamed287@gmail.com>
2023-10-10 12:37:33 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+//creating an anonymous function
+const returnFirstTwoDrivers = function(driver){
+    return driver.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(driver){
+    return driver.slice(2,4);
+}
+//creating a function that has two elements that were previously the function
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// creating a function that should give a value using created multiple times
+const createFareMultiplier = function(fareMultiplier){
+    return function(fare){
+        return fare * fareMultiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(driversArray, fcn) {
+    return fcn(driversArray);
+}
